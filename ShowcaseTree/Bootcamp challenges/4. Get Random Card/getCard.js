04f38c58d89a510ddb3b66e0cab23efd05e24c88
PaxDaciae <80334035+PaxDaciae@users.noreply.github.com>
2022-01-18 22:43:50 +0200

    Update getCard.js

--- ShowcaseTree/Bootcamp challenges/4. Get Random Card/getCard.js
+++ ShowcaseTree/Bootcamp challenges/4. Get Random Card/getCard.js
@@ -10,25 +10,11 @@
 // Return both as an object
 
 function getCard() {
-    // Here we generate the card template and define the basic arrays
-    // From which we will obtain the number and suit.
-    const playingCard = {
-        cardValue: "",
-        cardSuit: ""
-    }
     const cardNumberArray = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
     const cardSuitArray = ['clubs', 'spades', 'hearts', 'diamonds'];
-
-    // This f. generates a random number between 0 and maxNumber-1
-    function generateNumber(maxNumber) {
-        let number = Math.floor(Math.random()*maxNumber);
-        return number
+    function select(array) {
+        let index = Math.floor(Math.random()*array.length)
+        return array[index]
     }
-    // This line returns the number of the card from the numbers array.
-    console.log(cardNumberArray.length)
-    playingCard.cardValue = cardNumberArray[generateNumber(cardNumberArray.length)];
-    // This line returns the suit of the card from the suits array.
-    console.log(cardSuitArray.length)
-    playingCard.cardSuit = cardSuitArray[generateNumber(cardSuitArray.length)];
-    return playingCard;
+    return {number: select(cardNumberArray), suit: select(cardSuitArray)}
 }
