39358c9058177053fa399f3ce46f8b447aa72bc6
PaxDaciae <80334035+PaxDaciae@users.noreply.github.com>
2022-05-11 19:50:49 +0300

    In development

--- ShowcaseTree/Edabit Challenges Expert/Tic-Tac-Toe/TicTac.js
+++ ShowcaseTree/Edabit Challenges Expert/Tic-Tac-Toe/TicTac.js
@@ -8,13 +8,38 @@ const board = [
 ];
 
 function checkTick(board) {
+
+    function sign(rand,coloana){
+        return board[rand][coloana];
+    };
     for (rand=0; rand<board.length; rand++) {
         for (coloana=0; coloana<board[rand].length; coloana++) {
-            currentSign = board[rand][coloana];
-            // if (currentSign)
-            console.log(`pentru randul ${rand} si coloana ${coloana} simbolul este`, currentSign);
+            currentSign = sign(rand, coloana);
+            function checkHorizontal(rand, coloana) {
+                if (currentSign === sign(rand, (coloana+1)%3) === sign(rand, (coloana+2)%3))
+                return true;
+            }
+            function checkVertical(rand, coloana) {
+                if (currentSign === sign((rand+1)%3, coloana) === sign((rand+2)%3, coloana))
+                return true;
+            }
+            function checkDiagonal(rand, coloana) {
+                if (currentSign === sign((rand+1)%3, (coloana+1)%3) === sign((rand+2)%3, (coloana+2)%3))
+                return true;
+            }
+            if (checkHorizontal(rand, coloana) || checkVertical(rand, coloana) || checkDiagonal(rand, coloana)) console.log(currentSign);
+                // (currentSign === sign(rand+1,coloana) === sign() || currentSign === sign(rand, coloana+1) || currentSign === sign(rand+1, coloana+1)) 
+            // if (coloana < 0)
         }
-        // currentSign = board[i][i];
-        // console.log(`${0+i} este`, currentSign);
     }
-} 
+}
+
+            /** function checkTick(board) {
+//     for (rand=0; rand<board.length; rand++) {
+//         for (coloana=0; coloana<board[rand].length; coloana++) {
+//             currentSign = board[rand][coloana];
+//             // if (currentSign)
+//             console.log(`pentru randul ${rand} si coloana ${coloana} simbolul este`, currentSign);
+//         }
+//         // currentSign = board[i][i];
+//         // console.log(`${0+i} este`, currentSign); **/
