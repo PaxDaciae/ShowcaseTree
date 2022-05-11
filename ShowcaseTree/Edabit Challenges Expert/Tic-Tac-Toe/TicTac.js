8eabe6f74b3b3c9c6f759e3660bf02706a7f2e75
PaxDaciae <80334035+PaxDaciae@users.noreply.github.com>
2022-05-11 20:15:19 +0300

    Done & working!

--- ShowcaseTree/Edabit Challenges Expert/Tic-Tac-Toe/TicTac.js
+++ ShowcaseTree/Edabit Challenges Expert/Tic-Tac-Toe/TicTac.js
@@ -2,37 +2,46 @@
 // there are three X's in a row or returns "O" if there is three O's in a row.
 
 const board = [
-    ["O", "X", "O"],
+    ["X", "X", "O"],
     ["X", "X", "O"],
     ["O", "X", "X"]
 ];
 
 function checkTick(board) {
-
-    function sign(rand,coloana){
-        return board[rand][coloana];
-    };
     for (rand=0; rand<board.length; rand++) {
         for (coloana=0; coloana<board[rand].length; coloana++) {
-            currentSign = sign(rand, coloana);
+            function sign(rand, coloana) {
+                return board[rand][coloana];
+            }
+            currentSign = board[rand][coloana]
             function checkHorizontal(rand, coloana) {
-                if (currentSign === sign(rand, (coloana+1)%3) === sign(rand, (coloana+2)%3))
+                if (currentSign === sign(rand, (coloana+1)%3) && currentSign === sign(rand, (coloana+2)%3))
                 return true;
+                else return false;
             }
             function checkVertical(rand, coloana) {
-                if (currentSign === sign((rand+1)%3, coloana) === sign((rand+2)%3, coloana))
+                if (currentSign === sign((rand+1)%3, coloana) && currentSign === sign((rand+2)%3, coloana))
                 return true;
+                else return false;
             }
             function checkDiagonal(rand, coloana) {
-                if (currentSign === sign((rand+1)%3, (coloana+1)%3) === sign((rand+2)%3, (coloana+2)%3))
+                if (currentSign === sign((rand+1)%3, (coloana+1)%3) && currentSign === sign((rand+2)%3, (coloana+2)%3))
                 return true;
+                else return false;
             }
-            if (checkHorizontal(rand, coloana) || checkVertical(rand, coloana) || checkDiagonal(rand, coloana)) console.log(currentSign);
+            if (checkHorizontal(rand, coloana) || checkVertical(rand, coloana) || checkDiagonal(rand, coloana)) 
+            return true;
+            else return false;
                 // (currentSign === sign(rand+1,coloana) === sign() || currentSign === sign(rand, coloana+1) || currentSign === sign(rand+1, coloana+1)) 
             // if (coloana < 0)
         }
     }
+    const result = checkTick(board);
+    console.log(result);
 }
+
+const result = checkTick(board);
+
 
             /** function checkTick(board) {
 //     for (rand=0; rand<board.length; rand++) {
