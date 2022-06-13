c2cdb7f2c828cdb8d49b47ffecb2f1717ee84d23
PaxDaciae <80334035+PaxDaciae@users.noreply.github.com>
2022-06-13 17:55:45 +0300

    Update main.js

--- ShowcaseTree/HTML-CSS/Apps/Warhammer Hit Calculator/main.js
+++ ShowcaseTree/HTML-CSS/Apps/Warhammer Hit Calculator/main.js
@@ -19,6 +19,7 @@ const unit = {
             // Here we have the statlines for units in array form, simplified placeholder names for testing//
 const a = [5, 3, 2, 4, 5, 1, 3, 7, 3];
 const b = [6, 3, 3, 4, 3, 2, 2, 7, 3];
+    //c = [M,Ws,Bs, S, T,Wu, A,Ld, S];
 
             // This function creates a d6 dice roll//
 function diceRoll() { 
@@ -28,12 +29,20 @@ function diceRoll() {
 
             // This function merges the template object with the unit's statline array//
 function unitStatLineObject(array) {
-    const statLine = unit;
+    let statLine = {};
+    Object.assign(statLine, unit)
     for (i=0; i<9; i++) {
         statLine[Object.keys(statLine)[i]]=array[i];
         // console.log (`${[Object.keys(statLine)[i]]} = ${array[i]}`)
     }
     return statLine;
+}
+            // Used to debug unitStatLineObject function; //
+function statLinetest(array1, array2){
+    const unit1 = unitStatLineObject(array1)
+    const unit2 = unitStatLineObject(array2)
+    console.log(unit1);
+    console.log(unit2)
 }
 
 
@@ -45,7 +54,7 @@ function rollBucketOutcome(unit) {
     for (i = 0; i<numberAttacks; i++) {
         rollBucket.push(diceRoll());
     }
-    console.log(... rollBucket);
+    console.log("Zarurile sunt", ... rollBucket);
     return(rollBucket);
     
 }
@@ -64,21 +73,73 @@ function hitCalculator (attacker, cover) {
     return numberOfHits;
 }
 
-
             // This function calculates the number of damaging shots based on the nr of hits, attackers' strength, target's toughness.//
 function damagesCalculator(attacker, target, cover) {
     const attackerObject = unitStatLineObject(attacker);
     const targetObject = unitStatLineObject(target);
-    const numberOfHits = hitCalculator(attacker, target, cover);
-    console.log("stats sunt", attackerObject.strengh, targetObject.toughness)
-    console.log("hitcalc", hitCalculator(attackerObject, targetObject, cover))
-    var numberOfDamages = 0;
-    for (i = 0; i<= numberOfHits; i++) {
-        console.log("i este", i)
+    const numberOfHits = hitCalculator(attacker, cover);
+    var aStrength = parseInt(attackerObject.strengh);
+    var tTough = parseInt(targetObject.toughness);
+    console.log("stats pentru hit sunt", attackerObject.weaponSkill)
+    console.log("nr de hituri", numberOfHits);
+    console.log("A-strength", attackerObject.strengh, "T-tough", targetObject.toughness);
+
+    console.log(attackerObject)
+    console.log(targetObject)
+    var numberOfDamages = numberOfHits;
+    console.log("Strength e", aStrength)
+    console.log("Tough e", tTough)
+    if ((2*aStrength) <= tTough) {
+        console.log("2S<T")
     }
-    console.log(numberOfDamages);
-    return numberOfDamages;
+    else if (aStrength < tTough) {
+        console.log("S<T")
+
+    }
+    else if (aStrength == tTough) {
+        console.log("S=T")
+
+    }
+    else if (aStrength > tTough && !(aStrength >= 2*tTough)) {
+        console.log("S>T")
+
+    }
+    else if (aStrenght >= (2*tTough)) {
+        console.log("S>2T")
+
+    }
 }
+damagesCalculator(a, b, 0)
+
+
+
+
+
+
+// var damageRoll = diceRoll();
+// console.log ("i este", i, "rollul este", damageRoll);
+
+// }
+// for (i = 0; i<numberOfHits; i++) {
+// console.log("i este", i);
+
+
+
+
+
+            // Here we calculate saves//
+//         var saveRoll = diceRoll()
+//         if (saveRoll >= targetObject.save) {
+//             numberOfDamages --;
+//             console.log("i este", i, "save roll este", saveRoll, "save successful")}
+//             else {
+//                 console.log("i este", i, "save roll este", saveRoll, "save failed");
+//             }
+
+//     }
+//     console.log("numarul de damages", numberOfDamages);
+//     return numberOfDamages;
+// }
 
 
 
